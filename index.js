1ff39d7d8534e907733c0f49db5d6d17e5d65b1e
jjfayad16 <jfayad@gmail.com>
2022-12-04 18:47:59 -0300

    tratamento do pagamento

--- index.js
+++ index.js
@@ -53,7 +53,12 @@ app.post("/not", (req, res) => {
         MercadoPago.payment.search({
             qs: filtro
         }).then(data => {
-            console.log(data);
+            var pagamento = data.body.results[0];
+            if (pagamento != undefined) {
+                console.log(pagamento);
+            } else {
+                console.log("Pagamento não encontrado");
+            }
         }).catch(err => {
             console.log(err);
         })
